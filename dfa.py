263dcb3a81c9098a448c89a475b03804d2862a8b
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2021-09-15 23:35:09 +0800

    fix bug: fix dfa bug in level 1 detect

--- dfa.py
+++ dfa.py
@@ -6,36 +6,6 @@ class DFA:
     def __init__(self, keyword_list: list):
         self.state_event_dict = self._generate_state_event_dict(keyword_list)
 
-    def match(self, content: str):
-        match_list = []
-        state_list = []
-        temp_match_list = []
-
-        for char_pos, char in enumerate(content):
-            if char in self.state_event_dict:
-                state_list.append(self.state_event_dict)
-                temp_match_list.append({
-                    "start": char_pos,
-                    "match": ""
-                })
-
-            for index, state in enumerate(state_list):
-                if char in state:
-                    state_list[index] = state[char]
-                    temp_match_list[index]["match"] += char
-
-                    if state[char]["is_end"]:
-                        match_list.append(copy.deepcopy(temp_match_list[index]))
-
-                        if len(state[char].keys()) == 1:
-                            state_list.pop(index)
-                            temp_match_list.pop(index)
-                else:
-                    state_list.pop(index)
-                    temp_match_list.pop(index)
-
-        return match_list
-
     @staticmethod
     def _generate_state_event_dict(keyword_list: list) -> dict:
         state_event_dict = {}
@@ -57,3 +27,45 @@ class DFA:
                     current_dict["is_end"] = True
 
         return state_event_dict
+
+    def match(self, content: str):
+        match_list = []
+        state_list = []
+        temp_match_list = []
+        conlen = len(content)
+
+        for char_pos, char in enumerate(content):
+            if char in self.state_event_dict:
+                state_list.append(self.state_event_dict)
+                temp_match_list.append({
+                    "start": char_pos,
+                    "match": ""
+                })
+
+            for index, state in enumerate(state_list):
+                if char in state:
+                    state_list[index] = state[char]
+                    temp_match_list[index]["match"] += char
+
+                    if state[char]["is_end"]:
+                        # and (char_pos + 1 < and content[char_pos + 1] not in state[char])
+                        # if next char in state[char], then the match can not finish
+                        if not (char_pos + 1 < conlen and content[char_pos + 1] in state[char]):
+                            match_list.append(copy.deepcopy(temp_match_list[index]))
+                            # print(char_pos+1, content[char_pos + 1], state[char])
+
+                        # if the state[char] have no char, then delete this state
+                        if len(state[char].keys()) == 1:
+                            state_list[index] = None
+                            temp_match_list[index] = None
+                            # state_list.pop(index)
+                            # temp_match_list.pop(index)
+                else:
+                    state_list[index] = None
+                    temp_match_list[index] = None
+                    # state_list.pop(index)
+                    # temp_match_list.pop(index)
+            state_list = [i for i in state_list if i]
+            temp_match_list = [i for i in temp_match_list if i]
+
+        return match_list
--- keyCount.py
+++ keyCount.py
@@ -1,5 +1,16 @@
 from sys import argv
 import os
+import re
+from dfa import DFA
+
+
+keysList = ["auto", "break", "case", "char", "const", "continue",
+            "default", "do", "double", "else", "enum", "extern", "float",
+            "for", "goto", "if", "int", "long", "register", "return",
+            "short", "signed", "sizeof", "static", "struct", "switch",
+            "typedef", "union", "unsigned", "void", "volatile", "while"]
+
+targetsList = ["if", "else", "ifelse", "switch"]
 
 
 # read args
@@ -16,21 +27,46 @@ def readfile(path):
     if not os.path.exists(path):
         print("file not exists!")
         exit(1)
+
+    code = ""
     # openfile and return code
     with open(path, "r") as f:
-        code = f.read()
-        return code
+        codeList = f.readlines()
+    for i in codeList:
+        if not re.match("^\s*#include", i):
+            code += i.strip()+' '  # remove start space and end \n
+    code = re.sub("else if", "elseif", code)
+    code = re.sub("\"[^\"]*\"", "__STRING__", code)
+    return code
 
 
 # codeHandler
 def codeHandler(code, level):
-    print(code)
-    print(level)
+    ansList = []
+    if level >= 1:
+        ansList.append(countKeys(code))
+
+    output(ansList)
+    pass
+
+
+def countKeys(code):
+    dfa = DFA(keysList)
+    for i in dfa.match(code):
+        print(i)
+    return len(dfa.match(code))
+
+
+# output ans
+def output(ansList):
+    if len(ansList) >= 1:
+        print("total num: ", ansList[0])
     pass
 
 
 if __name__ == '__main__':
     path, level = argparse()
     code = readfile(path)
+    print(code)
     codeHandler(code, level)
     pass
